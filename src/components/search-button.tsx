3efacb8d826876698e1d60df254b91f716f1e7c5
Dhiraj Gagrai <dhirajgagrai@gmail.com>
2024-05-25 22:51:41 +0530

    Remove focus outline for search input & button

--- src/components/search-button.tsx
+++ src/components/search-button.tsx
@@ -40,7 +40,7 @@ const SearchButton = (): ReactElement => {
   const { pending } = useFormStatus()
   return (
     <Button
-      className="w-24 rounded-md bg-primary px-6 py-2 text-white shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary/50 dark:bg-primary dark:hover:bg-primary/90 dark:focus:ring-primary/50"
+      className="w-24 rounded-md bg-primary px-6 py-2 text-white shadow-sm transition-colors hover:bg-primary/90 dark:bg-primary dark:hover:bg-primary/90"
       type="submit"
     >{pending ? <LoadingSpinner /> : `Search`}</Button>
   )
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -9,7 +9,7 @@ const Search = (props: { value?: string }): ReactElement => {
   return (
     <form action={searchPlaylist} className="mt-8 flex items-center">
       <Input
-        className="mr-4 flex-1 rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary focus:outline-none focus:ring-2 focus:ring-primary dark:border-gray-700 dark:bg-gray-800 dark:text-gray-50 dark:focus:border-primary"
+        className="mr-4 flex-1 rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-50"
         placeholder="Paste YouTube playlist URL or ID"
         type="text"
         name="q"
