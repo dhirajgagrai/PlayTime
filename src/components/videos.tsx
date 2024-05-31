cdbb13030299a679f0e8ed702f08ab3845789145
Dhiraj Gagrai <dhirajgagrai@gmail.com>
2024-05-31 16:11:26 +0530

    Add button color for watched videos

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { CheckIcon, TrashIcon } from "lucide-react"
+import moment from "moment"
 import Link from "next/link"
 import { ReactElement, useState } from "react"
 
@@ -14,27 +15,32 @@ import { formatHrToDuration } from "@/lib/utils"
 
 const Videos = (props: { pId:string, pd: PlaylistDetails, fd: number[] }): ReactElement => {
   const [videos, setVideos] = useState(props.pd.videos)
-  const [totalDuration, setTotalDuration] = useState(props.fd.reduce((prev, curr) => prev += curr, 0))
+  const [totalDuration, setTotalDuration] = useState(props.fd.reduce(
+    (prev, curr) => moment.duration(prev, "hours").add(curr, "hours").asHours(),
+    0
+  ))
   const [watchedDuration, setWatchedDuration] = useState(0)
   const [watched, setWatched] = useState<string[]>([])
   const unavailableVideoCount = (props.pd?.videoCount as number) - (props.pd?.videos.length as number)
-  const progress = totalDuration ? Math.floor(watchedDuration / totalDuration * 100) : 0
+  const progress = totalDuration ? Math.round(watchedDuration / totalDuration * 100) : 0
 
-  const handleDelete = (duration: number, id: string, index: number) => {
+  const isChecked = (id: string): boolean => watched.some((element) => element === id)
+
+  const handleDelete = (duration: number, id: string, index: number): void => {
     if (watched.some((element) => element === id)) {
-      setWatchedDuration(watchedDuration - duration)
+      setWatchedDuration(moment.duration(watchedDuration, "hours").subtract(duration, "hours").asHours())
     }
-    setTotalDuration(totalDuration - duration)
+    setTotalDuration(moment.duration(totalDuration, "hours").subtract(duration, "hours").asHours())
     setVideos(videos.filter(element => element.id !== id))
     props.fd.splice(index, 1)
   }
 
-  const handleWatched = (duration: number, id: string) => {
+  const handleWatched = (duration: number, id: string): void => {
     if (watched.some((element) => element === id)) {
       setWatchedDuration(watchedDuration - duration)
       setWatched(watched.filter(element => element !== id))
     } else {
-      setWatchedDuration(watchedDuration + duration)
+      setWatchedDuration(moment.duration(watchedDuration, "hours").add(duration, "hours").asHours())
       setWatched([...watched, id])
     }
   }
@@ -46,7 +52,9 @@ const Videos = (props: { pId:string, pd: PlaylistDetails, fd: number[] }): React
           <Link href={`https://www.youtube.com/playlist?list=${props.pId}`} target="_blank" className="hover:underline">
             <div className="text-2xl font-bold mr-10">{props.pd?.title}</div>
           </Link>
-          <div className="text-gray-500">Total videos: {props.pd?.videos.length} {Boolean(unavailableVideoCount) && `(${unavailableVideoCount} unavailable)`}</div>
+          <div className="text-gray-500">
+            Total videos: {props.pd?.videos.length} {Boolean(unavailableVideoCount) && `(${unavailableVideoCount} unavailable)`}
+          </div>
           <div className="flex gap-2 mt-4 items-center">
             <Button className="text-gray-400 hover:text-gray-900 w-auto" size="icon" variant="ghost">
               <TrashIcon className="h-4 w-4 mr-1" /> Delete
@@ -85,11 +93,17 @@ const Videos = (props: { pId:string, pd: PlaylistDetails, fd: number[] }): React
                 </Link>
               </div>
               <div className="flex basis-48 items-center gap-2">
-                <Button className="text-gray-400 hover:text-gray-900" size="icon" variant="ghost" onClick={() => handleDelete(props.fd[i], video.id as string, i)}>
+                <Button
+                  className="text-gray-300 hover:text-gray-900" size="icon" variant="ghost"
+                  onClick={() => handleDelete(props.fd[i], video.id as string, i)}
+                >
                   <TrashIcon className="h-4 w-4" />
                   <span className="sr-only">Delete</span>
                 </Button>
-                <Button className="text-gray-400 hover:text-gray-900" size="icon" variant="ghost" onClick={() => handleWatched(props.fd[i], video.id as string)}>
+                <Button
+                  className={isChecked(video.id as string) ? "text-gray-900" : "text-gray-300" + " hover:text-gray-900"} variant="ghost"
+                  onClick={() => handleWatched(props.fd[i], video.id as string)}
+                >
                   <CheckIcon className="h-4 w-4" />
                   <span className="sr-only">Watched</span>
                 </Button>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@ import { type ClassValue, clsx } from "clsx"
 import moment from "moment"
 import { twMerge } from "tailwind-merge"
 
-export const cn = (...inputs: ClassValue[]) => {
+export const cn = (...inputs: ClassValue[]): string => {
   return twMerge(clsx(inputs))
 }
 
